78918063ffadc958a1b0bcc589feca847dc068b5
vladosrus <chikovvlad@yandex.ua>
2022-04-19 07:04:06 +0300

    некоторые изменения

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,17 @@ let popupJob = document.querySelector('.popup__input-job');
 let closeIcon = document.querySelector('.popup__close-icon');
 let formElement = document.querySelector('.popup__container');
 
-editButton.addEventListener('click', function () {
+function popupOpened() {
     popup.classList.add('popup_opened');
     popupName.value = name.textContent;
     popupJob.value = job.textContent;
-});
+}
+editButton.addEventListener('click', popupOpened);
 
-closeIcon.addEventListener('click', function () {
+function popupClose() {
     popup.classList.remove('popup_opened');
-});
+}
+closeIcon.addEventListener('click', popupClose);
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
