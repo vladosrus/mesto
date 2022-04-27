7bb2e78619685fdaeeff2b27057ad2305bba893e
Vlad Chikov <chikovvlad@yandex.com>
2022-04-27 15:24:40 +0300

    добавить отрисовку начальных карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,61 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let name = document.querySelector('.profile__title');
-let job = document.querySelector('.profile__subtitle')
-let popupName = document.querySelector('.popup__input_named_name');
-let popupJob = document.querySelector('.popup__input_named_job');
-let closeIcon = document.querySelector('.popup__close-icon');
-let formElement = document.querySelector('.popup__form');
+// Дом элементы
+
+const popup = document.querySelector(".popup");
+const popupName = document.querySelector(".popup__input_named_name");
+const popupJob = document.querySelector(".popup__input_named_job");
+const editButton = document.querySelector(".profile__edit-button");
+const name = document.querySelector(".profile__title");
+const job = document.querySelector(".profile__subtitle");
+const closeIcon = document.querySelector(".popup__close-icon");
+const formElement = document.querySelector(".popup__form");
+
+//Открытие попапа
 
 function popupOpened() {
-    popup.classList.add('popup_opened');
-    popupName.value = name.textContent;
-    popupJob.value = job.textContent;
+  popup.classList.add("popup_opened");
+  popupName.value = name.textContent;
+  popupJob.value = job.textContent;
 }
-editButton.addEventListener('click', popupOpened);
+editButton.addEventListener("click", popupOpened);
+
+//Закрытие попапа
 
 function popupClose() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
-closeIcon.addEventListener('click', popupClose);
+closeIcon.addEventListener("click", popupClose);
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    name.textContent = popupName.value;
-    job.textContent = popupJob.value;
-    popupClose();
+//Измение текста в профиле
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  name.textContent = popupName.value;
+  job.textContent = popupJob.value;
+  popupClose();
 }
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
+
+//Отрисовка начальных карточек
+
+const cardTemplate = document
+  .querySelector("#card")
+  .content.querySelector(".element");
+const cardContainer = document.querySelector(".elements__list");
+
+const generateCard = (item) => {
+  const newCard = cardTemplate.cloneNode(true);
+  const newCardTitle = newCard.querySelector(".element__title");
+  newCardTitle.textContent = item.name;
+  const newCardImage = newCard.querySelector(".element__image");
+  newCardImage.src = item.link;
+
+  return newCard;
+};
+
+const renderCards = (item) => {
+  cardContainer.append(generateCard(item));
+};
+
+initialCards.forEach((item) => {
+  renderCards(item);
+});
