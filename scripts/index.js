ceca328ce4f744f7aec23e7b507655bcbba2143d
vladosrus <chikovvlad@yandex.ua>
2022-04-29 14:12:23 +0300

    внести правки в js

--- scripts/index.js
+++ scripts/index.js
@@ -1,82 +1,79 @@
 // Дом элементы
-
 const popupProfile = document.querySelector(".popup_named_profile");
-const popupCard = document.querySelector(".popup_named_card");
-const popupZoom = document.querySelector(".popup_named_zoom");
 const popupProfileName = popupProfile.querySelector(".popup__input_named_name");
 const popupProfileJob = popupProfile.querySelector(".popup__input_named_job");
+const popupProfileForm = popupProfile.querySelector(".popup__form");
+const profileCloseIcon = popupProfile.querySelector(".popup__close-icon");
+
+const popupCard = document.querySelector(".popup_named_card");
 const popupCardImgname = popupCard.querySelector(".popup__input_named_imgname");
 const popupCardLink = popupCard.querySelector(".popup__input_named_link");
+const popupCardForm = popupCard.querySelector(".popup__form");
+const cardCloseIcon = popupCard.querySelector(".popup__close-icon");
+
+const popupZoom = document.querySelector(".popup_named_zoom");
+const popupZoomCloseIcon = popupZoom.querySelector('.popup__close-icon');
+const popupZoomImg = document.querySelector('.popup__image');
+const popupZoomText = document.querySelector('.popup__caption');
+
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const name = document.querySelector(".profile__title");
 const job = document.querySelector(".profile__subtitle");
-const popupZoomImg = document.querySelector('.popup__image');
-const popupZoomText = document.querySelector('.popup__caption');
-const profileCloseIcon = popupProfile.querySelector(".popup__close-icon");
-const cardCloseIcon = popupCard.querySelector(".popup__close-icon");
-const popupZoomCloseIcon = popupZoom.querySelector('.popup__close-icon');
-const popupProfileForm = popupProfile.querySelector(".popup__form");
-const popupCardForm = popupCard.querySelector(".popup__form");
 
 //Открытие попапа редактирования профиля
-
-function popupOpened(popup) {
+function openPopup(popup) {
   popup.classList.add("popup_opened");
 }
 
 editButton.addEventListener("click", () => {
-  popupOpened(popupProfile);
+  openPopup(popupProfile);
   popupProfileName.value = name.textContent;
   popupProfileJob.value = job.textContent;
 });
 
 //Закрытие попапа редактирования профиля
-
-function popupClose(popup) {
+function closePopup(popup) {
   popup.classList.remove("popup_opened");
 }
 
 profileCloseIcon.addEventListener("click", () => {
-  popupClose(popupProfile);
+  closePopup(popupProfile);
 });
 
 //Измение текста в профиле
-
 popupProfileForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  popupClose(popupProfile);
+  closePopup(popupProfile);
   name.textContent = popupProfileName.value;
   job.textContent = popupProfileJob.value;
 });
 
 //Открытие попопа добавления карточек
-
 addButton.addEventListener("click", () => {
-  popupOpened(popupCard);
+  openPopup(popupCard);
 });
 
 //Закрытие попапа добавления карточек
 cardCloseIcon.addEventListener("click", () => {
-  popupClose(popupCard);
+  closePopup(popupCard);
 });
 
-//Добавление новых карточек
-
+//Добавление карточек
 popupCardForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  cardContainer.prepend(generateNewCard(popupCardImgname, popupCardLink));
-  popupClose(popupCard);
+  cardContainer.prepend(generateNewCard({name: popupCardImgname.value, link: popupCardLink.value}));
+  closePopup(popupCard);
   evt.target.reset();
 });
 
-const generateNewCard = (a, b) => {
+const generateNewCard = ({name, link}) => {
   const newCard = cardTemplate.cloneNode(true);
   const newCardTitle = newCard.querySelector(".element__title");
-  newCardTitle.textContent = a.value;
+  newCardTitle.textContent = name;
   const newCardImage = newCard.querySelector(".element__image");
-  newCardImage.src = b.value;
-  newCardImage.alt = a.value;
+  newCardImage.src = link;
+  newCardImage.alt = name;
 
   //Функция лайков
   newCard
@@ -94,62 +91,26 @@ const generateNewCard = (a, b) => {
 
   //Увеличение фотографии
   newCardImage.addEventListener("click", (evt) => {
-    popupOpened(popupZoom);
+    openPopup(popupZoom);
     popupZoomImg.src = newCardImage.src;
+    popupZoomImg.alt = newCardImage.alt;
     popupZoomText.textContent = newCardTitle.textContent;
-  });
-
-  //Закрытие увеличенных фотографий
-  popupZoomCloseIcon.addEventListener("click", () => {
-    popupClose(popupZoom);
   });
 
   return newCard;
 };
-//Автодобавление начальных карточек
 
+//Закрытие увеличенных фотографий
+popupZoomCloseIcon.addEventListener("click", () => {
+  closePopup(popupZoom);
+});
+
+//Автодобавление начальных карточек
 const cardTemplate = document
   .querySelector("#card")
   .content.querySelector(".element");
 const cardContainer = document.querySelector(".elements__list");
 
-const generateInitialCard = (item) => {
-  const newCard = cardTemplate.cloneNode(true);
-  const newCardTitle = newCard.querySelector(".element__title");
-  newCardTitle.textContent = item.name;
-  const newCardImage = newCard.querySelector(".element__image");
-  newCardImage.src = item.link;
-  newCardImage.alt = item.name;
-
-  //Функция лайков
-  newCard
-    .querySelector(".element__like-botton")
-    .addEventListener("click", (evt) => {
-      evt.target.classList.toggle("element__like-botton_active");
-    });
-
-  //Функция удаления карточек
-  newCard
-    .querySelector(".element__basket-botton")
-    .addEventListener("click", (evt) => {
-      evt.target.closest(".element").remove();
-    });
-
-  //Увеличение фотографии
-  newCardImage.addEventListener("click", (evt) => {
-    popupOpened(popupZoom);
-    popupZoomImg.src = newCardImage.src;
-    popupZoomText.textContent = newCardTitle.textContent;
-  });
-
-  //Закрытие увеличенных фотографий
-  popupZoomCloseIcon.addEventListener("click", () => {
-    popupClose(popupZoom);
-  });
-
-  return newCard;
-};
-
 initialCards.forEach((item) => {
-  cardContainer.append(generateInitialCard(item));
+  cardContainer.append(generateNewCard(item));
 });
