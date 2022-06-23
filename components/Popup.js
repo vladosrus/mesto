9c7d404f9badf97d94be99ba2db2bd567bdfaa1c
Vlad Chikov <chikovvlad@yandex.com>
2022-06-23 16:55:13 +0300

    класс Popup готов

--- components/Popup.js
+++ components/Popup.js
@@ -1,29 +1,39 @@
-import { profileCloseIcon, popupProfile } from "../utils/constants.js";
+import {
+  profileCloseIcon,
+  popupZoomCloseIcon
+} from "../utils/constants.js";
 
 export default class Popup {
   constructor(popupCelector) {
-    this._popup = document.querySelector(popupCelector);
+    this._popup = popupCelector;
   }
 
-  open(popup) {
-    popup.classList.add("popup_opened");
+  open() {
+    this._popup.classList.add("popup_opened");
     document.addEventListener("keydown", this._handleEscClose);
   }
 
-  close(popup) {
-    popup.classList.remove("popup_opened");
+  close() {
+    this._popup.classList.remove("popup_opened");
     document.removeEventListener("keydown", this._handleEscClose);
   }
 
-  _handleEscClose(evt) {
+  _handleEscClose = (evt) => {
     if (evt.key === "Escape") {
-      const findPopup = document.querySelector(".popup_opened");
-      this.close(findPopup);
+      this.close();
     }
-  }
+  };
+
+  _handleClosePopup = (evt) => {
+    if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup_opened')) {
+        this.close()
+      };
+  };
 
   setEventListeners() {
-    profileCloseIcon.addEventListener("click", () => this.close(popupProfile));
+    profileCloseIcon.addEventListener("click", () => this.close(this._popup));
+    popupZoomCloseIcon.addEventListener("click", () => this.close(this._popup));
+    this._popup.addEventListener("click", this._handleClosePopup);
   }
 }
 
@@ -48,4 +58,18 @@ function closePopup(popup) {
 
 //Слушатель кнопки-крестика для закрытия попапа редактирования профиля
 profileCloseIcon.addEventListener("click", () => closePopup(popupProfile));
+
+// Закрытие попапов нажатием на оверлей
+const handleClosePopup = () => {
+  overlayList.forEach((overlay) => {
+    overlay.addEventListener("click", (evt) => {
+      const activePopup = document.querySelector(".popup.popup_opened");
+      if (activePopup === evt.target) {
+        closePopup(overlay);
+      }
+    });
+  });
+};
+handleClosePopup();
+
 */
